0a73388a2dc418e897cdc7c705295b1af41a269b
pranayXoX <pranay.dmnsns@gmail.com>
2022-12-18 12:57:54 +0530

    Basic setup

---
+++ mockito_sample_1/src/test/java/mockito_sample_1/test_1.java
@@ -0,0 +1,15 @@
+package mockito_sample_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test_1 {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
